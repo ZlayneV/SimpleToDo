40d057f62509e75b5190366b019d424e013b0e7f
zacka <zackary.volf@faulkner.edu>
2021-01-22 01:00:45 -0600

    intitial commit

---
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -0,0 +1,69 @@
+package com.example.simpletodo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+
+    public interface OnLongCLickListener {
+        void onItemLongCLicked(int position);
+    }
+
+    List<String> items;
+    OnLongCLickListener longCLickListener;
+
+    public ItemsAdapter(List<String> items, OnLongCLickListener longCLickListener) {
+        this.items = items;
+        this.longCLickListener = longCLickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+
+        return new ViewHolder (todoView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String item = items.get(position);
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    // Container to provide access to views that represent each row of text
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+
+        }
+
+        public void bind(String item) {
+
+            tvItem.setText(item);
+            tvItem.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    longCLickListener.onItemLongCLicked(getAdapterPosition());
+                    return true;
+                }
+            });
+        }
+    }
+}
